b3576e6e61b3379492a8b9029714c0ff8a655f98
isaias <isaias@hotmail.com>
2022-09-28 22:11:01 -0300

    sec

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,33 @@
 import{useContext}from 'react'
 import { Link } from 'react-router-dom';
 import { CartContext } from '../../context/cartContext'
-
+import moment from 'moment';
+import {collection,addDoc, getFirestore} from 'firebase/firestore'
 const Cart = () => {
     const{cart,removeItem}=useContext(CartContext);
-    console.log('cart',cart)
-  return (<div>
+    const createOrden = () =>{
+    const db =getFirestore();
+    const order = {
+      buyer:{
+        name:'juan',
+        phone:'11445522',
+        email:'juan@test.com'
+      },
+      items: cart,
+      total:cart.reduce((valorPasado,valorActual)=>valorPasado+(valorActual.price*valorActual.quantity),0),
+      date: moment().format(),
+    };
+     const query = collection(db,'orders');
+     addDoc(query,order)
+     .then(({id})=>{
+      console.log(id)
+      alert('felicidades por tu compra');})
+     .catch(()=>
+      alert('tu compra no pudo ser procesada')
+      )
+     
+  }
+  return (<div >
     <h1>Carrito</h1>
     {cart.length === 0 ? (
     <>
@@ -23,11 +45,15 @@ const Cart = () => {
             <button className="add-btn" onClick={()=> removeItem(Item.id)}>
                eliminar carrito
         </button>
-       </div>
+        </div>
+      
        
         ))}
+        <button onClick={createOrden} style={{marginTop:'25px'}}>crear orden</button>
         </>
+        
     )}
+    
   
     
   </div>);
